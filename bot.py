e0c7721d3934205d56edad044f2f0e71b4332727
kamran-114 <babayevkamran772@gmail.com>
2025-05-14 05:43:31 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -232,3 +232,4 @@ if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=f"https://tahastorebot.onrender.com/{BOT_TOKEN}")
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
