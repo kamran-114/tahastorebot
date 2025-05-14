e97a41a1160febb11b9e6ab919b727c26071cee1
kamran-114 <babayevkamran772@gmail.com>
2025-05-14 06:05:45 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -94,9 +94,9 @@ def get_weather(city):
 def handle_dialogs(text, chat_id):
     if any(word in text for word in ["salam", "salamm", "salam əleykum", "salam aleykum"]):
         bot.send_message(chat_id, "Əleykum Salam!")
-    elif "necəsən?" in text or "yaxşısan?" in text or "necəsən"
+    elif "necəsən?", in text or "yaxşısan?", in text or "necəsən",
         bot.send_message(chat_id, "Şükür mən yaxşıyam! Sən necəsən?")
-    elif "çox sağ ol" in text or "çox sağol" in text or "təşəkkür" in text or "yaxşıyam" in text or "Şükür Allaha salamatlıqdı":
+    elif "çox sağ ol", in text or "çox sağol", in text or "təşəkkür", in text or "yaxşıyam", in text or "Şükür Allaha salamatlıqdı":
         bot.send_message(chat_id, "Dəyməz, həmişə yaxşı ol! 😊", "həmişə salamatlıq olsun təki")
     elif any(word in text for word in ["qiymət", "neçəyə", "neçəyədır", "neçəyidir", "neçədir"]):
         bot.send_message(chat_id, "Qiymətlər kitabdan asılı olaraq dəyişir. Hansı kitabla maraqlanırsınız?")
